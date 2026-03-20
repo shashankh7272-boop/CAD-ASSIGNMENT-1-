04f21434c601c449fa776b4b7c4ef6cb25e9f516
shashankh7272-boop <shashankh7272@gmail.com>
2026-03-20 22:22:39 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calling the function
+result = add(num1, num2)
+
+# Displaying result
+print("Sum is:", result)
